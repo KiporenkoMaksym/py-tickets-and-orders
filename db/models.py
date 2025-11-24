9ad72a65450b87b3480cf4b60a48d19c1af3114e
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-11-24 20:04:42 +0200

    Complete3

--- db/models.py
+++ db/models.py
@@ -40,7 +40,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"<Order: {self.created_at}>"
+        return f"<Order: {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}>"
 
 
 class CinemaHall(models.Model):
@@ -90,10 +90,11 @@ class Ticket(models.Model):
         ]
 
     def __str__(self) -> str:
-        return (f"<Ticket: "
-                f"{self.movie_session.movie.title}"
-                f"{self.movie_session.show_time} (row: {self.row},"
-                f"seat: {self.seat})>")
+        show_time = self.movie_session.show_time.strftime('%Y-%m-%d %H:%M:%S')
+        return (
+            f"<Ticket: {self.movie_session.movie.title} "
+            f"{show_time} (row: {self.row}, seat: {self.seat})>"
+        )
 
     def clean(self) -> None:
         hall = self.movie_session.cinema_hall
--- services/user.py
+++ services/user.py
@@ -23,10 +23,11 @@ def create_user(
 
 
 def get_user(user_id: int) -> Optional[get_user_model()]:
-    user = get_user_model()
+    User = get_user_model()
     try:
-        return user.objects.get(id=user_id)
-    except user.DoesNotExist:
+        user = User.objects.get(id=user_id)
+        return user
+    except User.DoesNotExist:
         return None
 
 
@@ -37,8 +38,8 @@ def update_user(
         email: str = None,
         first_name: str = None,
         last_name: str = None) -> Optional[get_user_model()]:
-    user = get_user_model()
-    user = get_user_model().objects.filter(id=user_id).first()
+    User = get_user_model()
+    user = get_user(user_id)
     if not user:
         return None
 
